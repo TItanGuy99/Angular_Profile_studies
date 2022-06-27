b3eb843995be78d09e6a9f9d5ee85a92cc3dccb7
TItanGuy99 <luiz.neosnk@gmail.com>
2022-06-27 10:04:27 -0300

    Fixing ActivatedRoute.

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -10,19 +9,7 @@ export class HeaderComponent implements OnInit {
   savePage: number = 1;
   event!: string;
 
-  constructor(private Location: Location) {
-    if (this.Location.path().includes('interviews')) {
-      this.savePage = 2;
-    } else if (this.Location.path().includes('fanpages')) {
-      this.savePage = 3;
-    } else if (this.Location.path().includes('development')) {
-      this.savePage = 4;
-    } else if (this.Location.path().includes('contact')) {
-      this.savePage = 5;
-    } else {
-      this.savePage = 1;
-    }
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 
